f27bb862583962c49201bf05ec3c6a7d1f7593b9
Rohit <rkumar@centuryclientsupport.com>
2024-01-11 00:02:42 +0530

    make it better

--- index.js
+++ index.js
@@ -34,6 +34,9 @@ app.use(bodyParser.urlencoded({limit: '50mb'}));
 
 app.post('/api/prospect/create',(req, res) => {
     console.log('Got body ', req.body);
+    let dobParts = req.body.Date_Of_Birth.split('/');
+    let dobFormatted = `${dobParts[2]}-${dobParts[0].padStart(2, '0')}-${dobParts[1].padStart(2, '0')}`;
+
     let insertProspect = `
         INSERT INTO "Prospect" ("Loan_Amount", "Purpose", "Credit_Score", "Email", "Date_Of_Birth", "First_Name", "Last_Name",
                                 "Address1", "Address2", "Zip", "City", "Phone_Number", "State", "Home_Ownership_Status",
@@ -44,7 +47,7 @@ app.post('/api/prospect/create',(req, res) => {
 
     let Form_Id = generateUUID();
     client.query(insertProspect, [
-        req.body.Loan_Amount, req.body.Purpose, req.body.Credit_Score, req.body.Email, req.body.Date_Of_Birth,
+        req.body.Loan_Amount, req.body.Purpose, req.body.Credit_Score, req.body.Email, dobFormatted,
         req.body.First_Name, req.body.Last_Name, req.body.Address1, req.body.Address2, req.body.Zip, req.body.City, req.body.Mobile_Phone,
         req.body.State, req.body.Home_Ownership_Status, req.body.Education_Level, req.body.Employment_Status,
         req.body.Annual_Income, req.body.Payment_Frequency, req.body.Social_Security_Number, req.body.Company, Form_Id
@@ -60,6 +63,8 @@ app.post('/api/prospect/create',(req, res) => {
 
 app.post('/api/prospect/update',(req, res) => {
     console.log('Got body ', req.body);
+    let dobParts = req.body.Date_Of_Birth.split('/');
+    let dobFormatted = `${dobParts[2]}-${dobParts[0].padStart(2, '0')}-${dobParts[1].padStart(2, '0')}`;
     res.send(req.body);
     let updateProspect = `
         UPDATE "Prospect" SET (
@@ -72,7 +77,7 @@ app.post('/api/prospect/update',(req, res) => {
         WHERE "ProspectId" = $20
     `;
     client.query(updateProspect, [
-        req.body.Loan_Amount, req.body.Purpose, req.body.Credit_Score, req.body.Email, req.body.Date_Of_Birth,
+        req.body.Loan_Amount, req.body.Purpose, req.body.Credit_Score, req.body.Email, dobFormatted,
         req.body.First_Name, req.body.Last_Name, req.body.Address1, req.body.Address2, req.body.Zip, req.body.City, req.body.Mobile_Phone,
         req.body.State, req.body.Home_Ownership_Status, req.body.Education_Level, req.body.Employment_Status,
         req.body.Annual_Income, req.body.Payment_Frequency, req.body.Social_Security_Number, req.body.ProspectId
